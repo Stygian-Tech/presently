ad858c772299708cd27e10eb2439ebdbeabc8d5e
Sam Clemente <sam@theclementes.com>
2026-07-28 00:01:53 -0500

    Publish Marketing Site

---
+++ apps/web/astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from "astro/config";
+
+export default defineConfig({
+  output: "static",
+  site: process.env.PUBLIC_SITE_URL || "https://presently.photo",
+  trailingSlash: "always",
+});
